73efccfdb632d1eb55f0335bdd705bc7387531f8
kaysanf <kaysanf266@gmail.com>
2025-07-07 10:37:00 +0700

    Add files via upload

---
+++ db_kaysanf_d1a240060.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Jul 2025 pada 15.47
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kaysanf_d1a240060`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_about`
+--
+
+CREATE TABLE `tbl_about` (
+  `id_about` int(2) NOT NULL,
+  `about` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_artikel`
+--
+
+CREATE TABLE `tbl_artikel` (
+  `id_artikel` int(5) NOT NULL,
+  `nama_artikel` text NOT NULL,
+  `isi_artikel` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_gallery`
+--
+
+CREATE TABLE `tbl_gallery` (
+  `id_gallery` int(5) NOT NULL,
+  `judul` text NOT NULL,
+  `foto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_gallery`
+--
+
+INSERT INTO `tbl_gallery` (`id_gallery`, `judul`, `foto`) VALUES
+(1, 'flower', '1.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) CHARACTER SET armscii8 COLLATE armscii8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`username`, `password`) VALUES
+('admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_about`
+--
+ALTER TABLE `tbl_about`
+  ADD PRIMARY KEY (`id_about`);
+
+--
+-- Indeks untuk tabel `tbl_artikel`
+--
+ALTER TABLE `tbl_artikel`
+  ADD PRIMARY KEY (`id_artikel`);
+
+--
+-- Indeks untuk tabel `tbl_gallery`
+--
+ALTER TABLE `tbl_gallery`
+  ADD PRIMARY KEY (`id_gallery`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_about`
+--
+ALTER TABLE `tbl_about`
+  MODIFY `id_about` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_artikel`
+--
+ALTER TABLE `tbl_artikel`
+  MODIFY `id_artikel` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_gallery`
+--
+ALTER TABLE `tbl_gallery`
+  MODIFY `id_gallery` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+        $host = "localhost";
+        $user = "root";
+        $password = "";
+        $nama_database = "db_kaysanf_d1a240060";
+        $db = mysqli_connect($host, $user, $password, $nama_database);
